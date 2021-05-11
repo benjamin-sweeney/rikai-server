113642432179222b7367212453720441acf42910
Ben <benjamin.sweeney.r@gmail.com>
2021-05-10 18:26:17 -0700

    test type orm and basic char entity

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("hello World");
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+import { Character } from "./entities/Character";
+
+const main = async () => {
+  const conn = await createConnection();
+  // const char = conn.manager.create(Character, { romaji: "あ" });
+  // await conn.manager.save(char);
+  const chars = await conn.manager.find(Character);
+  console.log("chars: ", chars);
+};
+
+main().catch((err) => {
+  console.log(err);
+});
