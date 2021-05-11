1042d5c672e6629878670a30113fa5c038bc3661
Ben <benjamin.sweeney.r@gmail.com>
2021-05-11 12:03:55 -0700

    setup basic express/apollo server

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,36 @@
 import "reflect-metadata";
 import { createConnection } from "typeorm";
-import { Character } from "./entities/Character";
+// import { Character } from "./entities/Character";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
-  const conn = await createConnection();
+  // const conn = await createConnection();
 
   // create a character
   // const char = conn.manager.create(Character, { romaji: "あ" });
   // await conn.manager.save(char);
 
   // list all characters
-  const chars = await conn.manager.find(Character);
-  console.log("chars: ", chars);
+  // const chars = await conn.manager.find(Character);
+  // console.log("chars: ", chars);
+
+  const app = express();
+
+  const server = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+
+  server.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("server started on localhost:4000");
+  });
 };
 
 main().catch((err) => {
