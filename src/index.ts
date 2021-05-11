5c65a701c186eb667fd945632001c2d66ec3f734
Ben <benjamin.sweeney.r@gmail.com>
2021-05-11 10:18:41 -0700

    add comments

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,12 @@ import { Character } from "./entities/Character";
 
 const main = async () => {
   const conn = await createConnection();
+
+  // create a character
   // const char = conn.manager.create(Character, { romaji: "あ" });
   // await conn.manager.save(char);
+
+  // list all characters
   const chars = await conn.manager.find(Character);
   console.log("chars: ", chars);
 };
