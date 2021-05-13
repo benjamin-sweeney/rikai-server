e7bdc151277c0293799bdec4e1e68d4e94dbdbfc
Ben <benjamin.sweeney.r@gmail.com>
2021-05-12 18:31:01 -0700

    add me and logout, and finish login/register to user resolver

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,8 +1,18 @@
 import argon2 from "argon2";
 import { User } from "../entities/User";
-import { Arg, Field, Mutation, ObjectType, Resolver } from "type-graphql";
+import {
+  Arg,
+  Ctx,
+  Field,
+  Mutation,
+  ObjectType,
+  Query,
+  Resolver,
+} from "type-graphql";
 import { validateRegister } from "../utils/validateRegister";
 import { UsernamePasswordInput } from "./UsernamePasswordInput";
+import { MyContext } from "src/types";
+import { COOKIE_NAME } from "../constants";
 
 @ObjectType()
 class FieldError {
@@ -23,9 +33,19 @@ class UserResponse {
 
 @Resolver(User)
 export class UserResolver {
+  @Query(() => User, { nullable: true })
+  async me(@Ctx() { req }: MyContext) {
+    if (!req.session.userId) {
+      return null;
+    }
+    const user = await User.findOne(req.session.userId);
+    return user;
+  }
+
   @Mutation(() => UserResponse)
   async register(
-    @Arg("options") options: UsernamePasswordInput
+    @Arg("options") options: UsernamePasswordInput,
+    @Ctx() { req }: MyContext
   ): Promise<UserResponse> {
     const validationErrors = validateRegister(options);
     if (validationErrors) {
@@ -38,13 +58,16 @@ export class UserResolver {
       password: hashedPassword,
     }).save();
 
+    // login registered user
+    req.session.userId = user.id;
     return { user };
   }
 
   @Mutation(() => UserResponse)
   async login(
     @Arg("userName") userName: string,
-    @Arg("password") password: string
+    @Arg("password") password: string,
+    @Ctx() { req }: MyContext
   ): Promise<UserResponse> {
     // Check for user
     const user = await User.findOne({ username: userName });
@@ -71,7 +94,22 @@ export class UserResolver {
         ],
       };
     }
-
+    req.session.userId = user.id;
     return { user };
+  }
+
+  @Mutation(() => Boolean)
+  logout(@Ctx() { req, res }: MyContext) {
+    return new Promise((resolve) =>
+      req.session.destroy((err) => {
+        res.clearCookie(COOKIE_NAME); // May need to figure out if we want this on any result or just when a session is destroyed successfully
+        if (err) {
+          console.log(err);
+          resolve(false);
+          return;
+        }
+        resolve(true);
+      })
+    );
   }
 }
