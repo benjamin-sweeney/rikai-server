7b78748832a15452e033f6420548175fcbc3b962
Ben <benjamin.sweeney.r@gmail.com>
2021-05-15 14:37:48 -0700

    switch to ioredis

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,5 @@
 export const __prod__ = process.env.NODE_ENV === "production";
 export const COOKIE_NAME = "qid";
 export const SESSION_SECRET = "keyboard cat";
+export const CORS_ORIGIN = "http://localhost:3000";
+export const REDIS_URL = "127.0.0.1:6379";
--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,15 @@ import { buildSchema } from "type-graphql";
 import { MyContext } from "./types";
 import { CharacterResolver } from "./resolvers/character";
 import { UserResolver } from "./resolvers/user";
-import redis from "redis";
+import Redis from "ioredis";
 import session from "express-session";
 import connectRedis from "connect-redis";
-import { COOKIE_NAME, SESSION_SECRET, __prod__ } from "./constants";
+import {
+  COOKIE_NAME,
+  CORS_ORIGIN,
+  SESSION_SECRET,
+  __prod__,
+} from "./constants";
 import cors from "cors";
 
 const main = async () => {
@@ -19,11 +24,11 @@ const main = async () => {
   const app = express();
 
   const RedisStore = connectRedis(session);
-  const redisClient = redis.createClient();
-
+  const redisClient = new Redis();
+  app.set("with proxy", 1);
   app.use(
     cors({
-      origin: "http://localhost:4444",
+      origin: CORS_ORIGIN,
       credentials: true,
     })
   );
@@ -54,6 +59,7 @@ const main = async () => {
     context: ({ req, res }): MyContext => ({
       req,
       res,
+      redis: redisClient,
     }),
   });
 
